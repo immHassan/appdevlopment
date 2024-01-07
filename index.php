c817d2485b2c9c60b5c658b8c153a3861dfa55c7
Hassan.Muhammad <hassan.muhammad@cloudsolutions.com.sa>
2024-01-07 18:18:26 +0300

    home page creation

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Development</title>
+</head>
+<body>
+    <h2>
+<?php
+echo "APP DEVELOPMENT.PRO";
+?>
+</h2>
+
+</body>
+</html>
